862006da4f5af0c07170320306e8775412e8855b
rymate1234 <supermarioryan@gmail.com>
2015-10-12 23:47:59 +0000

    ton of stuff

--- main.js
+++ main.js
@@ -72,10 +72,13 @@ if (Meteor.isClient) {
             return Session.get("loaded");
         },
         hasNotifications: function () {
-            return (IRCPings.find({}, {sort: {date_time: -1}}).length != 0);
+            return (IRCPings.find({}, {sort: {date_time: -1}}).count() != 0);
         },
         notifications: function () {
             return IRCPings.find({}, {sort: {date_time: -1}});
+        },
+        notificationsCount: function () {
+            return IRCPings.find({}, {sort: {date_time: -1}}).count();
         }
     });
 
@@ -113,7 +116,8 @@ if (Meteor.isClient) {
     });
 
     Template.message.rendered = function(){
-        jQuery('#messages').scrollTop( jQuery('#messages').prop("scrollHeight") );
+        var messages = jQuery('#messages');
+        messages.scrollTop( messages.prop("scrollHeight") );
     };
 
     Template.channelmsg.rendered = function () {
@@ -128,16 +132,29 @@ if (Meteor.isClient) {
             if (keyCode == 9) {
                 e.preventDefault();
 
+                var words = input.value.split(" ");
+                var word  = words[words.length - 1];
+                var isFirst = words.length === 1;
+
                 var list = IRCUsers.find({channel: Session.get("currChannel")}, {sort: {ircuser_sorting: 1}});
 
                 list.forEach(function (user) {
-                    if (user.ircuser_norank.startsWith(input.value)) {
-                        input.value = user.ircuser_norank + ": ";
+                    if (word.length == 0) {
+                        return;
+                    }
+
+                    if (user.ircuser_norank.startsWith(word)) {
+                        if (isFirst) {
+                            input.value = user.ircuser_norank + ": ";
+                        } else {
+                            words[words.length - 1] = words[words.length - 1].replace(word, user.ircuser_norank);
+                            input.value = words.join(" ");
+                        }
                     }
                 });
             }
         });
-    }
+    };
 
     Template.serverconnect.events({
         "submit form": function(e, t) {
@@ -180,19 +197,31 @@ if (Meteor.isClient) {
             t.find("#msginput").value = "";
         },
         "click #tabbutton": function (e) {
-            var input = document.getElementById('msginput');
+            var words = input.value.split(" ");
+            var word  = words[words.length - 1];
+            var isFirst = words.length === 1;
+
             var list = IRCUsers.find({channel: Session.get("currChannel")}, {sort: {ircuser_sorting: 1}});
 
             list.forEach(function (user) {
-                if (user.ircuser_norank.startsWith(input.value)) {
-                    input.value = user.ircuser_norank + ": ";
+                if (word.length == 0) {
+                    return;
+                }
+
+                if (user.ircuser_norank.startsWith(word)) {
+                    if (isFirst) {
+                        input.value = user.ircuser_norank + ": ";
+                    } else {
+                        words[words.length - 1] = words[words.length - 1].replace(word, user.ircuser_norank);
+                        input.value = words.join(" ");
+                    }
                 }
             });
-        }
+        },
+        "click #addbutton": function (e) { document.getElementById('imageitModal').toggle() },
     });
 
     Template.loginform.events({
-
         'click #login': function(e, t) {
             e.preventDefault();
             // retrieve the input field values
@@ -267,7 +296,195 @@ if (Meteor.isClient) {
 
         }, 2000);
 
-    }
+    };
+
+    Template.imageit.rendered = function () {
+        document.getElementById('fileInput').addEventListener('change', submitForm);
+        document.getElementById('imgUpload').addEventListener('submit', onSubmit);
+        document.getElementById('paste').addEventListener('paste', handlePaste);
+        var msgInput = document.getElementById('msginput');
+
+        function submitForm() {
+            input = document.getElementById('submit');
+            if (input) {
+                input.click();
+            }
+        }
+
+        function onSubmit(e) {
+            console.log("caught it");
+            if (e.preventDefault) e.preventDefault();
+            var uploader = document.getElementById('fileInput');
+            if (uploader.files.length == 0) {
+                return false;
+            } else {
+                uploadFile(uploader.files[0]);
+            }
+            return false;
+        }
+
+        // Minified version of isMobile
+        (function (i) {
+            var e = /iPhone/i, n = /iPod/i, o = /iPad/i, t = /(?=.*\bAndroid\b)(?=.*\bMobile\b)/i, r = /Android/i, d = /BlackBerry/i, s = /Opera Mini/i, a = /IEMobile/i, b = /(?=.*\bFirefox\b)(?=.*\bMobile\b)/i, h = RegExp("(?:Nexus 7|BNTV250|Kindle Fire|Silk|GT-P1000)", "i"), c = function (i, e) {
+                return i.test(e)
+            }, l = function (i) {
+                var l = i || navigator.userAgent;
+                this.apple = {
+                    phone: c(e, l),
+                    ipod: c(n, l),
+                    tablet: c(o, l),
+                    device: c(e, l) || c(n, l) || c(o, l)
+                }, this.android = {
+                    phone: c(t, l),
+                    tablet: !c(t, l) && c(r, l),
+                    device: c(t, l) || c(r, l)
+                }, this.other = {
+                    blackberry: c(d, l),
+                    opera: c(s, l),
+                    windows: c(a, l),
+                    firefox: c(b, l),
+                    device: c(d, l) || c(s, l) || c(a, l) || c(b, l)
+                }, this.seven_inch = c(h, l), this.any = this.apple.device || this.android.device || this.other.device || this.seven_inch
+            }, v = i.isMobile = new l;
+            v.Class = l
+        })(window);
+
+        if (!isMobile.any) {
+            if (!window.Clipboard) {
+                var pasteCatcher = document.getElementById("paste");
+                // Firefox allows images to be pasted into contenteditable elements
+                pasteCatcher.setAttribute("contenteditable", "");
+                // as long as we make sure it is always in focus
+                pasteCatcher.focus();
+                document.addEventListener("click", function () {
+                    pasteCatcher.focus();
+                });
+            }
+        }
+
+        /* Handle paste events */
+        function handlePaste(e) {
+            // We need to check if event.clipboardData is supported (Chrome)
+            if (e.clipboardData) {
+                // Get the items from the clipboard
+                var items = e.clipboardData.items;
+                if (items) {
+                    // Loop through all items, looking for any kind of image
+                    for (var i = 0; i < items.length; i++) {
+                        if (items[i].type.indexOf("image") !== -1) {
+                            // We need to represent the image as a file,
+                            var blob = items[i].getAsFile();
+                            // and use a URL or webkitURL (whichever is available to the browser)
+                            // to create a temporary URL to the object
+                            var URLObj = window.URL || window.webkitURL;
+                            var source = URLObj.createObjectURL(blob);
+
+                            // The URL can then be used as the source of an image
+                            uploadFile(blob);
+                        }
+                    }
+                }
+                // If we can't handle clipboard data directly (Firefox),
+                // we need to read what was pasted from the contenteditable element
+            } else {
+                // This is a cheap trick to make sure we read the data
+                // AFTER it has been inserted.
+                console.log("Getting image from contenteditable");
+                setTimeout(checkInput, 1);
+            }
+        }
+
+        /* Parse the input in the paste catcher element */
+        function checkInput() {
+            // Store the pasted content in a variable
+            var child = pasteCatcher.childNodes[0];
+
+            // Clear the inner html to make sure we're always
+            // getting the latest inserted content
+            pasteCatcher.innerHTML = "";
+
+            if (child) {
+                // If the user pastes an image, the src attribute
+                // will represent the image as a base64 encoded string.
+                if (child.tagName === "IMG") {
+                    console.log("Got an image from contenteditable");
+                    createImage(child);
+                }
+            }
+        }
+
+        /* Creates a new image from a given source */
+        function createImage(source) {
+            source.crossOrigin = 'Anonymous';
+            console.log("Converting image to blob");
+            var blob = dataUriToBlob(source.src);
+            uploadFile(blob);
+        }
+
+
+        function dataUriToBlob(dataURI) {
+            // serialize the base64/URLEncoded data
+            var byteString;
+            if (dataURI.split(',')[0].indexOf('base64') >= 0) {
+                byteString = atob(dataURI.split(',')[1]);
+            } else {
+                byteString = unescape(dataURI.split(',')[1]);
+            }
+
+            // parse the mime type
+            var mimeString = dataURI.split(',')[0].split(':')[1].split(';')[0]
+
+            // construct a Blob of the image data
+            var array = [];
+            for (var i = 0; i < byteString.length; i++) {
+                array.push(byteString.charCodeAt(i));
+            }
+            return new Blob(
+                [new Uint8Array(array)], {
+                    type: mimeString
+                }
+            );
+        }
+
+        function uploadFile(file) {
+            var xhr = new XMLHttpRequest();
+
+            document.getElementById("paste").innerHTML = "<IMG SRC='http://images.rymate.co.uk/images/3SddFB8.gif'></img>";
+
+            xhr.upload.onprogress = function (e) {
+                //$(".progress").show();
+                var percentComplete = Math.ceil((e.loaded / e.total) * 100);
+                //$("#info_text").text("Uploaded: " + percentComplete + "%");
+                //$('.bar').width(percentComplete + "%");
+                console.log("Uploaded: " + percentComplete + "%");
+            };
+
+            xhr.onload = function () {
+                if (xhr.status == 200) {
+                    if (top.window) {
+                        console.log("window is avaliable");
+                    }
+                    console.log( "Window.location is " + top.window.location);
+                    addUploadedImage(xhr.response);
+                    //top.window.location.href = " + xhr.response;
+                    msgInput.value = msgInput.value + "http://images.rymate.co.uk/view/" + xhr.response;
+                } else if (xhr.status == 413) {
+                    alert("The image is too large! It must be 25MB or less.");
+                } else {
+                    alert("Something went wrong whilst uploading. This is probably a bug, so it'll be fixed soon!");
+                }
+            };
+
+            xhr.onerror = function () {
+                alert("Error! Upload failed. Can not connect to server.");
+            };
+
+            xhr.open("POST", "http://images.rymate.co.uk/upload", true);
+            xhr.setRequestHeader("Content-Type", file.type);
+            xhr.send(file);
+        }
+
+    };
 
     window.onresize = window.onload = function () {
         var vph = jQuery(window).height();
@@ -356,6 +573,7 @@ if (Meteor.isServer) {
         IRCConnections.remove({});
         IRCUsers.remove({});
         IRCChannels.remove({});
+        IRCPings.remove({});
     });
 
     Meteor.methods({
